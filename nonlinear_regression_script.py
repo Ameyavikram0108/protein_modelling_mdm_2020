f92aa31c3559e887ff538bb661c1a2a209fe9001
Nico Marrin <as19538@bristol.ac.uk>
2020-03-04 10:09:48 +0000

    More work on python nonlinear regression script

--- nonlinear_regression_script.py
+++ nonlinear_regression_script.py
@@ -4,10 +4,24 @@
 import numpy as np
 import representations as rep
 import mechanics as mc
+import scipy.optimize as sop
 
 rp = 42
 
 E = np.array([mc.get_energy(rp,sim) for sim in range(1,101)])
 
-P = np.array([rep.get_parameters(rp,sim) for sim in range(1,101)])
+P = np.array([rep.get_parameters(rp,sim,'C') for sim in range(1,101)])
+
+def rho(K):
+    """The function we want to minimize"""
+    return sum(E - ((P-np.mean(P,0))**2 @ K[1:] + K[0])**2)
+
+K0 = np.ones(10)
+
+print(rho(K0))
+
+Kopt = sop.minimize(rho,K0)['x']
+
+print(rho(Kopt))
+
 
