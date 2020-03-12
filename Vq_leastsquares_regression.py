bef1a588c4835bf1b8963f421aad1dec989ffa08
Nico Marrin <as19538@bristol.ac.uk>
2020-03-12 14:47:47 +0000

    Resetting quadratic regression script

--- Vq_leastsquares_regression.py
+++ Vq_leastsquares_regression.py
@@ -14,7 +14,7 @@ E = np.array([mc.get_energy(rp,sim) for sim in range(1,101)])
 
 P = np.array([rep.get_parameters(rp,sim,'CE') for sim in range(1,101)])
 
-gamma = 1.5
+gamma = 2
 
 def rho(K):
     """The function we want to minimize"""
@@ -45,15 +45,6 @@ plt.plot(xx,xx,'--k')
 # look into whether this is the right kind of correlation coefficient!
 print('Correlation Coefficient =',pearsonr(E,V_vals)[0])
 
-
-#Kw = 1.0e+03 * np.array([-2.8129, 0.0222,    0.0136,    0.0050,    0.0012,   -2.0370,
-#    -0.6188,   -1.0619, 0.1287,    0.0039 ])
-
-#V_vals2 = [V(P[sim],Kw) for sim in range(100)]
-
-#plt.scatter(E,V_vals2)
-#xx = np.linspace(min(V_vals),max(V_vals),1000)
-#plt.plot(xx,xx)
 ax = plt.gca()
 ax.set_aspect('equal')
 plt.show()
