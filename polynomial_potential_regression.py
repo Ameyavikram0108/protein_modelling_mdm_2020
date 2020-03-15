f869a6437f8d97e14243dfb7dd0d8a075c2834fa
Nico Marrin <as19538@bristol.ac.uk>
2020-03-15 17:46:28 +0000

    Added labels to regression scatter plot and introduced optimization of equilibrium values

--- polynomial_potential_regression.py
+++ polynomial_potential_regression.py
@@ -8,12 +8,12 @@ import representations as rep
 import mechanics as mc
 import scipy.optimize as sop
 import matplotlib.pyplot as plt
-from scipy.stats.stats import pearsonr
+from scipy import stats
 import time
 
 t0 = time.time()
 
-rp = 1
+rp = 42
 
 print('protein number: ',rp)
 
@@ -26,38 +26,53 @@ pm = np.mean(P,0)
 N = len(P[0])
 
 d = 4
+print('highest order of terms in polynomials:',d)
 
 def Vp(p,K,d):
     """A polynomial potential function of degree d with coefficients given by
     the vector K
     """
-    return K[0] + sum(abs(p-pm)**gamma @ K[N*(gamma-2) + 1 : N*(gamma-1) + 1] for gamma in range(2,d+1))
+    return K[0] + sum(abs(p-K[1:N+1])**gamma @ K[N*(gamma-1) + 1 : N*gamma + 1] for gamma in range(2,d+1))
 
 def rho(K):
     """The sum of squared errors across all simulations
     (which we attempt to minimize)
     """
-    return sum((E[sim] - Vp(P[sim],K,d))**2 for sim in range(100))
+    return sum(abs(E[sim] - Vp(P[sim],K,d))**2 for sim in range(100))
 
 
 
-K0 = np.ones((d-1)*N + 1)
+K0 = np.ones(d*N + 1)
+K0[0] = -2000
+K0[1:N+1] = pm
 
 Kopt = sop.minimize(rho,K0)['x']
 
 print('Optimized coefficients:')
 print(Kopt)
 
+print('Parameter means')
+print(pm)
+
+print('Differences between parameter means and calculated equilibria:')
+print(Kopt[1:N+1]-pm)
+
 print('Total error squared after optimization = ',rho(Kopt))
 
 V_vals = [Vp(P[sim],Kopt,d) for sim in range(100)]
+R2 = stats.linregress(E,V_vals)[2]**2
+print('R^2  =',R2)
 
-plt.scatter(E,V_vals)
+ax = (plt.figure()).add_axes([.1,.1,.8,.8])
+ax.scatter(E,V_vals,'rx')
 xx = np.linspace(min(V_vals),max(V_vals),1000)
-plt.plot(xx,xx,'--k')
+ax.plot(xx,xx,'--k')
+ax.set_xlabel(r"$E(\mathbf{p})$")
+ax.set_ylabel(r"$V(\mathbf{p})$")
+ax.set_title(r"""Scatter plot of predicted energy values against measured energy
+        values - Order ${}$ Polynomial Model for Protein ${}$ $R^2 =
+        {:.3f}$""".format(d,rp,R2))
 
-# look into whether this is the right kind of correlation coefficient!
-print('Correlation Coefficient =',pearsonr(E,V_vals)[0])
 
 ax = plt.gca()
 ax.set_aspect('equal')
