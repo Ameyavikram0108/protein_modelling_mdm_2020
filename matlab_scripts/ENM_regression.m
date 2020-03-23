daead37deb98f3915a2593c3695a78b36a70c9b7
Nico Marrin <as19538@bristol.ac.uk>
2020-03-23 19:57:20 +0000

    tidying up the ENM regression script

--- matlab_scripts/ENM_regression.m
+++ matlab_scripts/ENM_regression.m
@@ -4,26 +4,28 @@ E = csvread('rp42energies.csv');
 
 P = csvread('rp42params.csv');
 
+n = length(E);
+
 Pbar = mean(P,1);
 
 dP = P - Pbar;
 
-Q = dP.^2
-
-X = [ones(length(E),1) Q];
+X = [ones(n,1) dP.^2];
 
 [b,bint,r,rint,stats] = regress(E,X);
 
 % Print the calculated coefficients:
 b'
 
-% Print the calculated statistics (R^2, F-statistic, p-value, estimated error
-% variance)
+p = length(b);
+
+% Print the calculated statistics 
+% (R^2, F-statistic, p-value, estimated error, variance)
 stats
 
 R2 = stats(1);
 
 % The adjusted R^2 statistic:
-Rbar2 = 1 - (1-R2)*(length(E)-1)/(length(E) - length(b) -1)
+Rbar2 = 1 - (1-R2)*(n-1)/(n - p -1)
 
 scatter(X*b,E)
