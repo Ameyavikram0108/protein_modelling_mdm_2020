bb5a1fa1f1272d80546f984427525a4dd9e5a3c1
Nico Marrin <as19538@bristol.ac.uk>
2020-03-25 14:17:15 +0000

    Documentation for the generator file

--- generate_energy_param_datafiles.py
+++ generate_energy_param_datafiles.py
@@ -6,7 +6,12 @@ import numpy as np
 import representations as rps
 import mechanics as mc
 
-rp = 42 # random choice of protein
+# Choose protein number by editing this line:
+rp = 42 
+
+# Choose representation type by editing this line:
+# (default 'CE' = centroid-endpoint representation)
+rep = 'CE'
 
 enfilename = 'rp' + str(rp) + 'energies.csv'
 paramfilename = 'rp' + str(rp) + 'params.csv'
@@ -17,4 +22,4 @@ with open(enfilename,'w+') as enfile:
 
 with open(paramfilename,'w+') as paramfile:
     for sim in range(1,101):
-        paramfile.write(str(list(rps.get_parameters(rp,sim,rep='CE')))[1:-1] + '\n')
+        paramfile.write(str(list(rps.get_parameters(rp,sim,rep=rep)))[1:-1] + '\n')
